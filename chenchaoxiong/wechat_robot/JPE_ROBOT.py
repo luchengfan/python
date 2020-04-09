510de36adb270d1bd1ebca20c7f46bf7ad17d8d3
chenchaoxiong <chenchaoxiong@cvte.com>
2020-04-09 23:58:56 +0800

    [feature][python][add remind to push order]
    [what]add remind to push order
    [why]need to push order everyday
    [how]use "python3 JPE_ROBOT.py 下推订单"

--- chenchaoxiong/wechat_robot/JPE_ROBOT.py
+++ chenchaoxiong/wechat_robot/JPE_ROBOT.py
@@ -1,12 +1,19 @@
 import requests
+import sys
 
-url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=fa8f77c0-c1a5-4e80-89ae-8a8c10f09128"
 headers = {"Content-Type": "text/plain"}
-s = "What do you want to say? "
+robot = sys.argv[1]
+if robot == "下推订单":
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=54a2ea2c-942f-4adb-98ee-966d92694879"
+    cmd = "大家下推订单啦，超过12点就超时了！"
+else:
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=5f4ba177-7032-41dd-ab47-26d600cb61ea"
+    cmd = "执行报错啦,帅雄还不赶紧看下~"
 data = {
       "msgtype": "text",
       "text": {
-         "content": s,
+         "content": cmd,
+         "mentioned_list":["@all"],
       }
    }
 r = requests.post(url, headers=headers, json=data)
