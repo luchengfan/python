9d19cee3b387c14db36c7ff5bfe0167d6c78cd0a
luchengfan <lufan199204@qq.com>
2020-05-17 09:38:42 +0800

    [feature][python][modify function for send_email]send_email
    [what] 修改发送邮件功能，修改为同时发送给多个邮箱
    [why] 修改成同时发给多个人
    [how] 使用python发送邮件

--- luchengfan/send_email/send_email.py
+++ luchengfan/send_email/send_email.py
@@ -5,7 +5,7 @@ from email.header import Header
 from_addr = '287485391@qq.com'
 qqCode = '***'
 # 输入收件人地址:
-to_addrs =  'luchengfan92@163.com'   #'lufan199204@qq.com'
+to_addrs =  ['luchengfan92@163.com','lufan199204@qq.com']
 # 输入SMTP服务器地址:
 smtp_server = "smtp.qq.com"
 
@@ -15,16 +15,16 @@ server.set_debuglevel(1)
 server.login(from_addr, qqCode)
 
 #组装发送内容
-message = MIMEText('这是我发送的第一封邮件', 'plain', 'utf-8')   #发送的内容
+message = MIMEText('同时发给卧听风雨和163邮箱', 'plain', 'utf-8')   #发送的内容
 message['From'] = Header("咫尺天涯", 'utf-8')   #发件人
-message['To'] = Header("卧听风雨", 'utf-8')     #收件人
+message['To'] = ','.join(to_addrs)       #这里必须要把多个邮箱按照逗号拼接为字符串
 subject = 'Python SMTP 邮件测试'
 message['Subject'] = Header(subject, 'utf-8')  #邮件标题
 
 try:
-    server.sendmail(from_addr, [to_addrs], message.as_string())
+    server.sendmail(from_addr, to_addrs, message.as_string())
+    print('邮件发送成功')
 except Exception as e:
     print('邮件发送失败--' + str(e))
 
-print('邮件发送成功')
 server.quit()
