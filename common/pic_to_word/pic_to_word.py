1f4715caefa11838deda4b64e593e1b8975956ac
luchengfan <luchengfan@cvte.com>
2020-05-19 18:05:32 +0800

    [feature][python][modify pic_to_word.py]pic_to_word
    [what] 修改识别图片中的问题
    [why] 修改功能
    [how] 屏蔽识别英文

--- common/pic_to_word/pic_to_word.py
+++ common/pic_to_word/pic_to_word.py
@@ -5,8 +5,11 @@ from PIL import Image
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = r'F:\Program Files\Tesseract-OCR\tesseract.exe'
-text_eng = pytesseract.image_to_string(Image.open('python.png'))
-print(text_eng)
 
+#识别英文
+#text_eng = pytesseract.image_to_string(Image.open('python.png'))
+#print(text_eng)
+
+#识别中文
 text_chi = pytesseract.image_to_string(Image.open('denggao.jpg') , lang = 'chi_sim')
 print("\n" , text_chi)
