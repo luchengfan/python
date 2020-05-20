787b8a7de1dfd8b9d8d2e76ce1a54ae2bfeb9d28
luchengfan <luchengfan@cvte.com>
2020-05-20 20:37:49 +0800

    [feature][python][modify function of clean_at_string]modify clean_at_string
    [what] 修改clean_at_string功能，添加对item类型的识别
    [why] xml文件中有item类型
    [how] 增加对item类型的替换

--- luchengfan/clean_at_string/clean_at_string.py
+++ luchengfan/clean_at_string/clean_at_string.py
@@ -8,14 +8,21 @@ def clean_string():
     #得到文档元素对象
     root = dom.documentElement
     strings = root.getElementsByTagName('string')
+    items = root.getElementsByTagName('item')
 
     for i in range(len(strings)):
-        #xml_string[strings[i].getAttribute('name')] = strings[i].firstChild.data
         if '@string/' in strings[i].firstChild.data:
             temp_string = strings[i].firstChild.data[8:]
             for j in range(len(strings)):
                 if temp_string == strings[j].getAttribute('name'):
                     strings[i].firstChild.data = strings[j].firstChild.data
+
+    for i in range(len(items)):
+        if '@string/' in items[i].firstChild.data:
+            temp_string = items[i].firstChild.data[8:]
+            for j in range(len(strings)):
+                if temp_string == strings[j].getAttribute('name'):
+                    items[i].firstChild.data = strings[j].firstChild.data
 
     #修改并保存文件
     xml_specific = 'result.xml'
