c819b2f68c944549f935b7ccdb31b6f16b5eca48
luchengfan <lufan199204@qq.com>
2020-05-21 23:39:37 +0800

    [feature][python][delete empty in xml]empty string
    [what] 删除xml文件中的<item></item>
    [why] <item></item>为空内容
    [how] 过滤<item></item>

--- luchengfan/clean_at_string/clean_at_string.py
+++ luchengfan/clean_at_string/clean_at_string.py
@@ -1,16 +1,28 @@
 import xml.dom.minidom
 
-def clean_string():
-    xml_file = 'temp_strings.xml'
+xml_file = 'strings.xml'
+strings_del_empty = 'strings_del_empty.xml'
 
+def del_empty_string():
+    fo = open(strings_del_empty , 'w' , encoding="utf-8")
+    fi = open(xml_file,'r', encoding="utf-8")
+    content = fi.readlines()
+    for line in content:
+        line=line.replace('<item></item>','')
+        fo.write(line)
+    fo.close()
+
+
+def clean_string():
     #打开xml文档
-    dom = xml.dom.minidom.parse(xml_file)
+    dom = xml.dom.minidom.parse(strings_del_empty)
     #得到文档元素对象
     root = dom.documentElement
     strings = root.getElementsByTagName('string')
     items = root.getElementsByTagName('item')
 
     for i in range(len(strings)):
+        print(strings[i].firstChild.data)
         if '@string/' in strings[i].firstChild.data:
             temp_string = strings[i].firstChild.data[8:]
             for j in range(len(strings)):
@@ -30,4 +42,5 @@ def clean_string():
         dom.writexml(f)
 
 if __name__ == "__main__":
+    del_empty_string()
     clean_string()
