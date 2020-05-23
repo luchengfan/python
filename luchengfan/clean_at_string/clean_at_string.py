944c8fd135b3e864b5fcc8ad8c7762a78cec18f3
luchengfan <luchengfan@cvte.com>
2020-05-23 17:03:53 +0800

    [feature][python][modify replace @string in xml]replace @string
    [what] 完善替换xml中@string字符的功能
    [why] 完善功能
    [how] 修改一些变量名以及增加一些提示，允许使用者输入需要修改的文件

--- luchengfan/clean_at_string/clean_at_string.py
+++ luchengfan/clean_at_string/clean_at_string.py
@@ -6,74 +6,81 @@
 <string name="wifi_security_psk_generic">@string/wifi_security_wpa_wpa2</string>
 则wifi_security_psk_generic的翻译同wifi_security_wpa_wpa2的翻译
 
-xml_file：原始文件，即翻译中的strings.xml
-strings_del_empty：生成的中间文件没删除了空元素
-xml_remove_at_string：最终生成的文件
+init_xml_file：带@string字符的原始文件，即翻译中的strings.xml
+del_empty_str_file：生成的中间文件，删除了原始文件中的空元素
+remove_at_str_file：最终生成的文件
 '''
 
 import xml.dom.minidom
+import os
 
-xml_file = 'strings.xml'
-strings_del_empty = 'strings_del_empty.xml'
-xml_remove_at_string = 'result.xml'
+del_empty_str_file = 'del_empty_str.xml'
+remove_at_str_file = 'result.xml'
 
-def del_empty_string():
+def del_empty_string(init_file):
     '''
     删除xml文件中的空元素，例如:
     <string name="empty" translatable="false"></string>
     <item></item>
     '''
-    input_file = open(xml_file ,'r' , encoding="utf-8")
-    del_empty_string_file = open(strings_del_empty , 'w' , encoding="utf-8")
+    input_file = open(init_file ,'r' , encoding="utf-8") #要处理的带@string字符的原始文件
+    output_file = open(del_empty_str_file , 'w' , encoding="utf-8") #生成的中间文件，删除了原始文件中的空元素
 
-    strings = input_file.readlines()
-    for line in strings:
+    all_strings = input_file.readlines()
+    for line in all_strings:
         if ('<item></item>' not in line) and (r'"></string>' not in line):
-            del_empty_string_file.write(line)
-    del_empty_string_file.close()
+            output_file.write(line)
+    output_file.close()
 
-def get_at_string_value(temp_string):
+def get_at_string_content(at_string):
     '''
     获取@string/后面的字符元素，例如：
     <string name="MODULATION_AUTO">@string/auto</string>
     则返回auto
     '''
-    string1 = temp_string.split('@string/')[1]
-    string2 = string1.split('<')[0]
-    return string2
+    temp_string = at_string.split('@string/')[1] #得到：auto</string>
+    content = temp_string.split('<')[0] #得到：auto
+    return content
 
-def get_string_value(temp_str):
+def get_string_value(string_content):
     '''
     通过temp_str获取到temp_str对应的翻译，例如temp_str为auto
     <string name="auto">Auto</string>
     则返回Auto
     备注：如果使用firstChild.data则需要删除掉xml中的空元素(即使用del_empty_string生成的文件)，否则会报data有Nonetype的错误
     '''
-    #打开xml文档
-    dom = xml.dom.minidom.parse(strings_del_empty)
-    #得到文档元素对象
-    root = dom.documentElement
+    dom = xml.dom.minidom.parse(del_empty_str_file) #打开xml文档
+    root = dom.documentElement #得到文档元素对象
     strings = root.getElementsByTagName('string')
 
     for i in range(len(strings)):
-        if temp_str == strings[i].getAttribute('name'):
+        if string_content == strings[i].getAttribute('name'):
             return strings[i].firstChild.data
 
-def del_at_string():
+def replace_at_string():
     '''
     替换xml文件中的@string字符串
     '''
-    open_del_empty_file = open(strings_del_empty ,'r' , encoding="utf-8")
-    result_file = open(xml_remove_at_string , 'w' , encoding="utf-8")
+    open_del_empty_file = open(del_empty_str_file ,'r' , encoding="utf-8")
+    result_file = open(remove_at_str_file , 'w' , encoding="utf-8")
 
-    content = open_del_empty_file.readlines()
-    for strings in content:
+    contents = open_del_empty_file.readlines()
+    for strings in contents:
         if '@string/' in strings:
-            str1 = get_at_string_value(strings)
-            str2 = get_string_value(str1)
+            str1 = get_at_string_content(strings) #获取@string/后面的字符元素
+            str2 = get_string_value(str1) #获取@string/后面字符对应的翻译
             strings = strings.replace('@string/' + str1 , str2)
         result_file.write(strings)
     result_file.close()
 
 if __name__ == "__main__":
-    del_at_string()
+    init_xml_file = input('请输入处理的xml文件：') 
+
+    if not os.path.isfile(init_xml_file):
+        print('文件不存在！！！')
+    else:
+        del_empty_string(init_xml_file)
+        replace_at_string()
+        print(del_empty_str_file , '为删除了原始文件中的空元素生成的文件')
+        print('\n处理完成!请查看' , remove_at_str_file)
+        print('\n请将{}和{}进行对比验证！'.format(init_xml_file , remove_at_str_file))
