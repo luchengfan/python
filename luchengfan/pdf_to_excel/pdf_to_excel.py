350a9f2e59d64b8585464f69a4b3b3d3202096a6
luchengfan <luchengfan@cvte.com>
2020-05-26 20:12:07 +0800

    [feature][python][modify function of pdf change to excel]
    [what] 修改pdf转化为excel的功能
    [why] 优化代码
    [how] 整理代码

--- luchengfan/pdf_to_excel/pdf_to_excel.py
+++ luchengfan/pdf_to_excel/pdf_to_excel.py
@@ -1,37 +1,49 @@
 # -*- coding: utf-8 -*-
 """
+功能描述：将pdf文件转化为excel文件
 请确保你在运行这个代码的时候，已经安装了pdfplumber库
-如果没有安装，请在[附件-命令提示符]下输入：
-pip install pdfplumber
+如果没有安装，请在[附件-命令提示符]下输入：pip install pdfplumber
 """
 
 import pdfplumber
 import xlwt
+import os
 
-# 定义保存Excel的位置
-workbook = xlwt.Workbook()  #定义workbook
-sheet = workbook.add_sheet('Sheet1')  #添加sheet
-i = 0 # Excel起始位置
+def is_empty(str):
+    '''
+    字符串是否为None或者为空
+    '''
+    if (str is None) or (str == ''):
+        return True
+    else:
+        return False
 
-path = input("请输入PDF文件位置：")    
-pdf = pdfplumber.open(path)
-print('\n开始读取数据\n')
+def pdf_to_excel():
+    # 定义保存Excel的位置
+    workbook = xlwt.Workbook()  #定义workbook
+    sheet = workbook.add_sheet('彩迅需求表')  #添加sheet
+    # Excel起始位置
+    i = 0
 
-for page in pdf.pages:
-    # 获取当前页面的全部文本信息，包括表格中的文字
-    # print(page.extract_text())                     
-    for table in page.extract_tables():
-        # print(table)
-        for row in table:
-            print(row)
-            for j in range(len(row)):
-                sheet.write(i, j, row[j])
-            i += 1
-        print('---------- 分割线 ----------')
+    pdf = pdfplumber.open(pdf_file)
+    print('\n开始读取数据\n')
 
-pdf.close()
+    for page in pdf.pages:
+        for table in page.extract_tables():
+            for row in table:
+                for j in range(len(row)):
+                    sheet.write(i, j, row[j])
+                i += 1
+    pdf.close()
 
-# 保存Excel表
-workbook.save('PDFresult.xls')
-print('\n写入excel成功')
+    # 保存Excel表
+    workbook.save('PDFresult.xls')
+    print('\n写入excel成功')
 
+if __name__ == '__main__':
+    pdf_file = 'TK2005-105.pdf' #input("请输入需要转化的PDF文件：")
+
+    if not os.path.isfile(pdf_file):
+        print('文件不存在！！！')
+    else:
+        pdf_to_excel()
