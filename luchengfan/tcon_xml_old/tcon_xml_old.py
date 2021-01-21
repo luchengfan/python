e3f18409f793621e5bbe98cd69b7e1a0e1f9e872
luchengfan <luchengfan@cvte.com>
2021-01-21 18:02:51 +0800

    使用可视化串口生成xml

--- luchengfan/tcon_xml_old/tcon_xml_old.py
+++ luchengfan/tcon_xml_old/tcon_xml_old.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 import os
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showinfo
 
-if __name__ == "__main__":
-    SOC_filename = input('请输入日期SOC软件的文件名：')
+def run():
+    SOC_filename = file_text.get()
     if '.bin' in SOC_filename:
         SOC_filename = SOC_filename.rstrip('.bin')
+
+    if 'SOC' not in SOC_filename:
+        showinfo(title='提示', message='请使用SOC软件生成xml')
+        file_text.set('') #清除文件内容
+        return 0
 
     xml_path = SOC_filename + '.xml'
 
@@ -28,4 +36,23 @@ if __name__ == "__main__":
     tcon_xml.write(' '*4 + '</Confirmation>' + '\n')
     tcon_xml.write('</Root>' + '\n')
 
-    print('xml文件已生成，请查看' + xml_path + '文件')
+    tips_message = 'xml文件已生成，请查看' + xml_path + '文件'
+    showinfo(title='提示', message=tips_message)
+    file_text.set('') #清除文件内容
+
+if __name__ == "__main__":
+    #可视化界面
+    frameT = Tk()
+    frameT.geometry('500x100+400+200')
+    frameT.title('TCON xml')
+    frame = Frame(frameT)
+    frame.pack(padx=10 , pady=10) #设置外边框
+    frame1 = Frame(frameT)
+    frame1.pack(padx=10 , pady=10) #设置外边框
+
+    file_text = StringVar()
+
+    ent = Entry(frame,width=50,textvariable=file_text).pack(fill=X,side=LEFT) #X方向填充，靠左
+    ext = Button(frame1,width=10,text='开始',font=('宋体',14),command=run).pack(fill=X,side=LEFT)
+    etb = Button(frame1,width=10,text='退出',font=('宋体',14),command=frameT.quit).pack(fill=Y,padx=10)
+    frameT.mainloop()
